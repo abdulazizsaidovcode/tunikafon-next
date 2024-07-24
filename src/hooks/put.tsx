3255494a1c1fec5a7425d90b8bde72a3e2784472
MingboyevAlisher2009 <mingboyevalisher171@gmail>
2024-07-24 11:41:06 +0500

    usePut hook

---
+++ src/hooks/put.tsx
@@ -0,0 +1,28 @@
+import axios from '../service/api';
+import { useState } from 'react';
+
+const usePut = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
+  const [data, setData] = useState<any>(null);
+
+  const put = async (url: string, id: string, updateData: {}) => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.put(`${url}/${id}`, updateData);
+      if (data.success) {
+        setData(data);
+      } else {
+        setError('Feild to Fetch');
+      }
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { data, error, isLoading, put };
+};
+
+export default usePut;
