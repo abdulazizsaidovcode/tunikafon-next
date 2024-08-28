796ea500a8f9289ad99c777990aceb004533f285
MingboyevAlisher2009 <mingboyevalisher171@gmail>
2024-08-28 12:02:22 +0500

    calculation

--- src/pages/Calculation.tsx
+++ src/pages/Calculation.tsx
@@ -315,8 +315,8 @@ const Calculation = () => {
   };
 
   const getProductDetails = async (id: number) => {
-    await getProductDetail(`product/details/${id}`);
-    const orderDetail = productdetail.map((product: any) => ({
+     const data = await getProductDetail(`product/details/${id}`);
+    const orderDetail = data.map((product: any) => ({
       detailId: product.id,
       name: product.name,
       attachmentId: product.attachmentId,
@@ -815,14 +815,15 @@ const Calculation = () => {
                   type="date"
                 />
               </div>
-              <div className="w-full">
+              <div className="w-full mb-2">
+                <h1 className='mb-2'>Guruh</h1>
                 <Menu
                   dismiss={{
                     itemPress: false,
                   }}
                 >
                   <MenuHandler>
-                    <div className="w-full cursor-pointer rounded flex gap-2 border overflow-y-auto border-black text-black p-2 mt-3 text-start font-normal">
+                    <div className="w-full cursor-pointer rounded flex gap-2 border overflow-y-auto border-black text-black p-2 text-start font-normal">
                       {groupssName && groupssName.length
                         ? groupssName.map((item: any) => (
                             <p className="border rounded line-clamp-1">
@@ -1032,7 +1033,7 @@ const Calculation = () => {
               </div>
             </div>
             <h1 className="text-lg">
-              {totalPrice ? formatNumberWithSpaces(totalPrice) : '0'}
+              {totalPrice ? formatNumberWithSpaces(totalPrice) : '0'} sum
             </h1>
             <div className="w-full flex justify-end gap-5">
               <Button onClick={toggleModal} color="red">
@@ -1045,7 +1046,7 @@ const Calculation = () => {
                 }}
                 color="green"
               >
-                {saveLoading ? 'Yuklanyapti...' : 'Saqlash'}
+                {saveLoading ? 'Yuklanyapti...' : 'Hisoblash'}
               </Button>
             </div>
           </div>
