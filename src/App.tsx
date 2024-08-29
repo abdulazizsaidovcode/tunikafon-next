9c06bf1059789672970fb78eb587665c099b9db3
abdulvohid31 <abdulvohidcode@gmail.com>
2024-08-29 10:00:51 +0500

    qd

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,7 @@ function App() {
                 />
               </Routes>
             </DefaultLayout>
-          ) : role === 'ROLE_ADMIN' && (
+          ) : role === 'ROLE_EMPLOYEE' && (
             <EmployeeRoute>
               <Routes>
                 <Route
