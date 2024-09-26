f4995e1c7f9b505a1324aac02252d16328547136
abdulvohid31 <abdulvohidcode@gmail.com>
2024-09-26 12:14:48 +0500

    xz

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -124,7 +124,7 @@ const ECommerce: React.FC = () => {
       <div className="w-full mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <div className="col-span-12">
           <ChartOne />
-          <ChartTwo month={month} year={year} />
+          {/* <ChartTwo month={month} year={year} /> */}
           <ChartThree month={month} year={year} />
         </div>
       </div>
